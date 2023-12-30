e28261570d27ac6c56830c6a033f3bd74aa47137
WilliamAlkawaz <william.alkawaz@gmail.com>
2023-12-30 12:57:44 +1100

    wip

--- SmartEmployment.DataAccess/Model/SmartEmploymentContext.cs
+++ SmartEmployment.DataAccess/Model/SmartEmploymentContext.cs
@@ -41,7 +41,7 @@ namespace SmartEmployment.DataAccess.Model
 			if (!optionsBuilder.IsConfigured)
 			{
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-				optionsBuilder.UseSqlServer("Server=tcp:smartemployment.database.windows.net,1433;Initial Catalog=SmartEmployment;Persist Security Info=False;User ID=william;Password=Timnewp77@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+				optionsBuilder.UseSqlServer("Server=tcp:smartemployment.database.windows.net,1433;Initial Catalog=SmartEmployment;Persist Security Info=False;User ID=william;Password=Pa$$w0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 			}
 			optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 			base.OnConfiguring(optionsBuilder);
